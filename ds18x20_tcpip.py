cf508443bdae0a77948d4112de03b0bea918a9b8
skusie <skusie@archlinux.arch>
2022-01-19 14:52:27 +0100

    put global constants into one dictionary at the beginning of the source code, for making it configurable more easily

--- ds18x20_tcpip.py
+++ ds18x20_tcpip.py
@@ -2,7 +2,15 @@ import network, socket, onewire, time, ds18x20
 import ntptime
 from machine import Pin, RTC
 
-def read_pw_file(filename):
+
+CONSTANTS = {	'ds18x20_pin_number' :  15,
+		'server_ip_address'  : '192.168.145.11',	
+		'creds_file_name'    : 'creds.txt',
+		'ntphost'            : 'fritz.box'
+	     }
+		
+
+def read_wlan_file(filename):
 	filehandle = open(filename, 'r')
 	data = filehandle.read().split(',')
 	filehandle.close()
@@ -11,7 +19,8 @@ def read_pw_file(filename):
 	return ssid, pw
 
 def connect_wlan():
-	ssid, pw = read_pw_file('creds.txt')
+	global CONSTANTS
+	ssid, pw = read_wlan_file(CONSTANTS['creds_file_name'])
 	wlan = network.WLAN(network.STA_IF)
 	wlan.active(True)
 	if not wlan.isconnected():
@@ -23,22 +32,28 @@ def connect_wlan():
 	print('network config:', wlan.ifconfig())
 
 def connect_server_and_send_data(command):
+	global CONSTANTS
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(('192.168.145.11', 5007))
+	s.connect((CONSTANTS['server_ip_address'], 5007))
 	s.send(command.encode())
 	print(s.recv(1))
 	s.close()
 
 def get_temperature_data():
-	ow = onewire.OneWire(Pin(15))
+	ow = onewire.OneWire(Pin(CONSTANTS['ds18x20_pin_number']))
 	ds = ds18x20.DS18X20(ow)
 	roms = ds.scan()
 	ds.convert_temp()
-	time.sleep_ms(1500)
+	time.sleep_ms(750)
 	temp_data = ''
+	first_iteration = True
 	try:
 		for rom in roms:
-			temp_data += zfill_special(str(ds.read_temp(rom)), 4) + ', '
+			if first_iteration:
+				first_iteration = False
+				temp_data += zfill_special(str(ds.read_temp(rom)), 4)
+			else:
+				temp_data += ',' + zfill_special(str(ds.read_temp(rom)), 4)
 	except:
 		temp_data = get_temperature_data()
 
@@ -62,7 +77,7 @@ def zfill_special(string, width):
 connect_wlan()
 rtc = RTC()
 print(rtc.datetime())
-ntptime.host='fritz.box'
+ntptime.host=CONSTANTS['ntphost']
 ntptime.settime()
 while True:
 	temps = get_temperature_data()
