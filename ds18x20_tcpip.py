c6ed90207f78a79e665a42b01a714037cc9e2857
skusie <skusie@archlinux.arch>
2022-01-19 07:09:52 +0100

    code can now read and send ds18x20 temperature data

--- ds18x20_tcpip.py
+++ ds18x20_tcpip.py
@@ -1,16 +1,17 @@
 import network, socket, onewire, time, ds18x20
-from ntptime
+import ntptime
 from machine import Pin, RTC
 
 def read_pw_file(filename):
 	filehandle = open(filename, 'r')
-	ssid = filehandle.readline()
-	pw   = filehandle.readline()
+	data = filehandle.read().split(',')
 	filehandle.close()
+	ssid = data[0]
+	pw   = data[1][0:-1]
 	return ssid, pw
 
 def connect_wlan():
-	ssid, pw = read_pw_file('wlan_credentials.txt')
+	ssid, pw = read_pw_file('creds.txt')
 	wlan = network.WLAN(network.STA_IF)
 	wlan.active(True)
 	if not wlan.isconnected():
@@ -23,13 +24,13 @@ def connect_wlan():
 
 def connect_server_and_send_data(command):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(('192.168.145.34', 5006))
+	s.connect(('192.168.145.11', 5007))
 	s.send(command.encode())
 	print(s.recv(1))
 	s.close()
 
 def get_temperature_data():
-	ow = onewire.OneWire(Pin(27))
+	ow = onewire.OneWire(Pin(15))
 	ds = ds18x20.DS18X20(ow)
 	roms = ds.scan()
 	ds.convert_temp()
@@ -37,21 +38,38 @@ def get_temperature_data():
 	temp_data = ''
 	try:
 		for rom in roms:
-			temp_data += str(ds.read_temp(rom)) + ', '
+			temp_data += zfill_special(str(ds.read_temp(rom)), 4) + ', '
 	except:
 		temp_data = get_temperature_data()
+
 	return(temp_data)
+
+
+def zfill_special(string, width):
+	string  = string.split('.')
+	string0 = string[0]
+	string1 = string[1]
+	
+	if len(string0) < width:
+		string0 = ("0" * (width - len(string0))) + string0 
+
+	if len(string1) < width:
+		string1 = string1 + "0" * (width - len(string1))
+
+	return (string0 + '.' + string1)
+
 
 connect_wlan()
 rtc = RTC()
+print(rtc.datetime())
 ntptime.host='fritz.box'
 ntptime.settime()
-print(rtc.datetime())
 while True:
 	temps = get_temperature_data()
 	print(temps)
 	message = str(rtc.datetime()) + ':' + temps
 	message_length = len(message)
 	header  = 'data:' + str((5 + len(message) + len(str(message_length))))	
+	print(header+message)
 	connect_server_and_send_data(header + message)
 	time.sleep_ms(1000)
